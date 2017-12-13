59f18a9f5f60ba7090bec393798453fe4debfe35
Eden Liu <eden90267@gmail.com>
2017-12-13 12:31:38 +0800

    Initial

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   context: path.join(__dirname, 'src'),
@@ -23,6 +24,33 @@ module.exports = {
         use: 'babel-loader',
         include: path.resolve('src'),
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader' // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader' // translates CSS into CommonJS
+          },
+          {
+            loader: 'less-loader' // compiles Less to CSS
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
       }
     ]
   },
@@ -39,6 +67,7 @@ module.exports = {
     alias: { // 從模組之中單獨取出特定的檔案進行打包放在 vendors 裡面，這樣做可以省去打包過多的檔案
       'react$': 'react/dist/react.min.js',
       'react-dom$': 'react-dom/dist/react-dom.min.js',
+      Source: __dirname + '/src',
     }
   }
 };
