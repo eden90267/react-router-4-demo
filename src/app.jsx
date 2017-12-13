1f185f9e4003681bfefa0c56a71f10a1b3f100f7
Eden Liu <eden90267@gmail.com>
2017-12-13 13:24:19 +0800

    Simple demo

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import {BrowserRouter, Link, Route} from "react-router-dom";
+
+
+require('Source/less/theme.less');
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Topics = () => (
+  <div>
+    <h2>Topics</h2>
+  </div>
+);
 
 ReactDOM.render(
-  <h1>Hello React！</h1>,
+  (
+    <BrowserRouter>
+      <div className="ui raised segment">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/topics">Topics</Link></li>
+        </ul>
+
+        <hr/>
+
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/topics" component={Topics}/>
+      </div>
+    </BrowserRouter>
+  ),
   document.querySelector('#app')
 );
